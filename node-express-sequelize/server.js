3958e8ebae65669c3d327943c5b2f93e73434a9d
ammarlio <ammarhalbouni@gmail.com>
2018-04-14 09:53:55 +0300

    assessment5

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -2,13 +2,20 @@ var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
-
+var body = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+app.get('/users',function(req,res){
+	User.findAll().then(users => {
+  		res.send(JSON.parse(users));
+})
+});
 
 
 
